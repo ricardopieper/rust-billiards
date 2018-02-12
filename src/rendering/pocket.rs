76c4f85c4ef7b6793fe0bc106858a9c482abe3a0
Ricardo Pieper <ricardopieper@live.com>
2018-02-12 05:29:09 -0200

    Rendering on resize was super broken

--- src/rendering/pocket.rs
+++ src/rendering/pocket.rs
@@ -6,8 +6,8 @@ use rendering::drawing::rgb;
 pub fn render_pockets(pool: &Pool, c: &Context, gl: &mut GlGraphics) {
     for pocket in pool.pockets.iter() {
         let r = pocket.radius * pool.play_area.aspect();
-        let position = pocket.position.to_screen_point_from_rect(&pool.play_area);
-        let radius = ellipse::circle(position.x, 2.0 * position.y, r);
+        let position = pocket.position.to_screen_point_relative(&pool.play_area);
+        let radius = ellipse::circle(position.x, position.y, r);
         let black = rgb(0.0, 0.0, 0.0, 1.0);
         ellipse(black, radius, c.transform, gl);
     }
